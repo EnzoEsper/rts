9f37193e8971aca2569f9a7080537d39eb81a191
enzoesper <enzoesper@hotmail.com>
2021-02-10 02:46:18 -0300

    ts with class based components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import UserSearch from './classes/UserSearch';
 import EventComponent from './events/EventComponent';
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'John', age: 35 },
+  { name: 'Michael', age: 27 },
+];
 
 const App = () => {
   return(
     <div>
-      <EventComponent />
+      <UserSearch users={users}/>
     </div>
   )
 }
